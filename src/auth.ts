632e32c3948c377bf62ea049baf40502f7fe9096
Abdullah <abdullah.khalil123321@gmail.com>
2025-07-12 03:21:29 +0500

    fix

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,7 @@ import Credentials from 'next-auth/providers/credentials';
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.BACKEND_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
 
