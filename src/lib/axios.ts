2279b8a80e51561a22df0ff1b0e5c786652c5f8f
Abdullah <abdullah.khalil123321@gmail.com>
2025-07-12 03:08:37 +0500

    fix

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getSession } from 'next-auth/react';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.BACKEND_URL,
   withCredentials: true,
 });
 
